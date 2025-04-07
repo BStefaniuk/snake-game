e7686bcaab71951aebd1be2d79e29ad780427033
Bartosz <s27929@pjwstk.edu.pl>
2025-04-07 23:51:20 +0200

    Zastosowanie układu kartezjańskiego zamiast exelowskiego oraz złączenie do jednego pliku engine.py

--- snake_project/tests/test_extended.py
+++ snake_project/tests/test_extended.py
@@ -1,16 +1,26 @@
-from game_engine.engine import move_snake_wrap, calculate_speed
+from game_engine.engine import move_snake_wrap, increase_speed
 
 def test_move_snake_wrap_left():
-    snake = [(0, 2)]
+    snake = [(0, 2)] #krawedz lewa
     new_snake = move_snake_wrap(snake, "left", (5, 5))
-    assert new_snake[0] == (4, 2)
+    assert new_snake[0] == (4, 2) #pojawia sie po prawej
+
+def test_move_snake_wrap_right():
+    snake = [(4,3)] #krawedz prawa
+    new_snake = move_snake_wrap(snake, "right", (5,5))
+    assert new_snake[0] == (0,3) #pojawia sie po lewej
 
 def test_move_snake_wrap_up():
-    snake = [(3, 0)]
+    snake = [(3, 0)] #krawedz gorna
     new_snake = move_snake_wrap(snake, "up", (5, 5))
-    assert new_snake[0] == (3, 4)
+    assert new_snake[0] == (3, 4) #pojawia sie na dole
+
+def test_move_snake_wrap_down():
+    snake = [(2,0)] #krawedz dolna
+    new_snake = move_snake_wrap(snake, "down", (5,5))
+    assert new_snake[0] == (2,4) #pojawia sie na gorze
 
 def test_calculate_speed_increase():
     speed = 1.0
-    new_speed = calculate_speed(speed)
-    assert round(new_speed, 3) == 1.07  # +7%
+    new_speed = increase_speed(speed)
+    assert round(new_speed, 3) == 1.07  # +7% speedu
