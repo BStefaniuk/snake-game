cfbeee6f0b5d1f29fa7d8b3888d1ebdffd65bd3e
Gabriela <s28751@pjwstk.edu.pl>
2025-04-08 22:33:47 +0200

    Utworzenie nowej klasy i metody

--- snake_project/ui/console_ui.py
+++ snake_project/ui/console_ui.py
@@ -2,3 +2,18 @@
 
 import tkinter as tk
 from game_engine.engine import init_game_status, update_game_status
+
+class SnakeGameGUI:
+
+    def __init__(self, root):
+        self.root = root
+        self.canvas_size = 400
+        self.cell_size = 20
+        self.board_size = (20, 20)
+        self.status = init_game_status(self.board_size)
+
+        self.canvas = tk.Canvas(root, width = self.canvas_size, height=self.canvas_size, bg="white")
+        self.canvas.pack()
+
+        self.root.bind("<Key>", self.handle_key)
+        self.update_ui()
